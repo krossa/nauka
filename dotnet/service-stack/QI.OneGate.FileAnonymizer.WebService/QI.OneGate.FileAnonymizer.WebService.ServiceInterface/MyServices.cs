8c68cd1ce83efba6194799dae450821aa4f66105
krossa <karol.rossa@gmail.com>
2019-03-22 18:54:53 +0100

    File Anonymizer template

---
+++ dotnet/service-stack/QI.OneGate.FileAnonymizer.WebService/QI.OneGate.FileAnonymizer.WebService.ServiceInterface/MyServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ServiceStack;
+using QI.OneGate.FileAnonymizer.WebService.ServiceModel;
+
+namespace QI.OneGate.FileAnonymizer.WebService.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
