605e9de0e84a939e46e50e59ff7ca319e1680250
krossa <karol.rossa@gmail.com>
2019-07-02 16:30:36 +0200

    Add section 7 project

--- angular/cooking-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ angular/cooking-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Ingridient } from 'src/app/shared/ingridient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('amountInput') amountInput: ElementRef;
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @Output() ingridientAdded = new EventEmitter<Ingridient>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAdd() {
+    const item = new Ingridient(this.nameInput.nativeElement.value, this.amountInput.nativeElement.value);
+    this.ingridientAdded.emit(item);
+  }
 }
