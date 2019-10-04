d4a70881905af9713579696cc75e557be855a141
krossa <karol.rossa@gmail.com>
2019-10-04 17:35:56 +0200

    angular alert window

--- angular/cooking-app/src/app/app.module.ts
+++ angular/cooking-app/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import { AuthComponent } from './auth/auth.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
+import { AlertComponent } from './shared/alert/alert.component';
 
 @NgModule({
   declarations: [
@@ -39,7 +40,8 @@ import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinne
     EmptyRecipeComponent,
     RecipeEditComponent,
     AuthComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
