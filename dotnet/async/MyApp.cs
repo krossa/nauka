4d33b06218ec73033a566410d9e9feda837139d8
krossa <karol.rossa@gmail.com>
2019-10-04 10:13:16 +0200

    update dotnet async

--- dotnet/async/MyApp.cs
+++ dotnet/async/MyApp.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Linq;
+using async;
 
 class MyApp
 {
-    public async void MyAsync()
+    public async Task MyAsync()
     {
-        System.Console.Write("A");
+        Console.WriteLine($"PROCESS(2) id: {Thread.GetCurrentProcessorId()}");
+        Console.WriteLine($"TAKS(2) id: {Thread.CurrentThread.ManagedThreadId}");
+        System.Console.WriteLine("A");
         await System.Threading.Tasks.Task.Delay(2000);
-        System.Console.Write("C");
+        // foreach (var item in Enumerable.Range(0, 50)) Console.WriteLine("x");
+        // await Task.Run(() =>
+        // {
+        //     Console.WriteLine($"TAKS(3) id: {Thread.GetCurrentProcessorId()}");
+        //     foreach (var item in Enumerable.Range(0, Program.count)) Console.WriteLine("1");
+        //     // Thread.Sleep(2000);
+        // });
+        System.Console.WriteLine("C");
+        Console.WriteLine($"PROCESS(2) id: {Thread.GetCurrentProcessorId()}");
+        Console.WriteLine($"TAKS(2) id: {Thread.CurrentThread.ManagedThreadId}");
     }
 
     public async void MyTaskAsync()
     {
-        string result = await MyTask();
+        Console.WriteLine($"PROCESS(2) id: {Thread.GetCurrentProcessorId()}");
+        Console.WriteLine($"TAKS(2) id: {Thread.CurrentThread.ManagedThreadId}");
+
+        var task = MyTask();
+        System.Console.WriteLine("Z");
+        var result = await task;
         System.Console.Write(result);
     }
 
@@ -20,7 +39,13 @@ class MyApp
     {
         return Task.Run<string>(() =>
         {
-            Thread.Sleep(2000);
+            Console.WriteLine($"PROCESS(3) id: {Thread.GetCurrentProcessorId()}");
+            Console.WriteLine($"TAKS(3) id: {Thread.CurrentThread.ManagedThreadId}");
+
+            System.Console.WriteLine("TASK RUN");
+            // Thread.Sleep(1000);
+            // foreach (var item in Enumerable.Range(0, 10)) Console.WriteLine("P");
+            System.Console.WriteLine("TASK END");
             return "Y";
         });
     }
--- dotnet/async/Program.cs
+++ dotnet/async/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace async
 {
     class Program
     {
+        public static int count = 50;
         static void Main(string[] args)
         {
             // Example1();
@@ -15,32 +17,51 @@ namespace async
 
         }
 
-        private static void Example1()
+        private static async void Example1()
         {
+            Console.WriteLine($"PROCESS(1) id: {Thread.GetCurrentProcessorId()}");
+            Console.WriteLine($"TAKS(1) id: {Thread.CurrentThread.ManagedThreadId}");
+
             new MyApp().MyAsync();
-            System.Console.Write("B");
+            // foreach (var item in Enumerable.Range(0, count)) Console.WriteLine("0");
+            Thread.Sleep(1000);
+            System.Console.WriteLine("B");
+            Console.WriteLine($"PROCESS(1) id: {Thread.GetCurrentProcessorId()}");
+            Console.WriteLine($"TAKS(1) id: {Thread.CurrentThread.ManagedThreadId}");
+
             System.Console.ReadKey();
         }
 
-        private static void Example2()
+        private static async Task Example2()
         {
+            Console.WriteLine($"PROCESS(1) id: {Thread.GetCurrentProcessorId()}");
+            Console.WriteLine($"TAKS(1) id: {Thread.CurrentThread.ManagedThreadId}");
+
             new MyApp().MyTaskAsync();
-            System.Console.Write("X");
+            Console.WriteLine($"PROCESS(1) id: {Thread.GetCurrentProcessorId()}");
+            Console.WriteLine($"TAKS(1) id: {Thread.CurrentThread.ManagedThreadId}");
+
+            // foreach (var item in Enumerable.Range(0, 10)) Console.WriteLine(item);
+            System.Console.WriteLine("X");
             System.Console.ReadKey();
         }
 
         private static async Task Example3Execute()
         {
+            Console.WriteLine($"TAKS(1) id: {Thread.CurrentThread.ManagedThreadId}");
             Console.WriteLine("pow samll");
             double d = await PowTwo(5);
             System.Console.WriteLine(d);
+            Console.WriteLine($"TAKS(1) id: {Thread.CurrentThread.ManagedThreadId}");
             Console.WriteLine("pow big");
             d = await PowTwo(10);
             System.Console.WriteLine(d);
+            Console.WriteLine($"TAKS(1) id: {Thread.CurrentThread.ManagedThreadId}");
         }
 
         private static async void Example3()
         {
+            Console.WriteLine($"TAKS(0) id: {Thread.CurrentThread.ManagedThreadId}");
             await Example3Execute();
             System.Console.ReadKey();
 
@@ -48,9 +69,12 @@ namespace async
 
         private static async ValueTask<double> PowTwo(double a)
         {
+            Console.WriteLine($"TAKS(2) id: {Thread.CurrentThread.ManagedThreadId}");
             if (a < 10 && a > -10) { return System.Math.Pow(a, 2); }
+            Console.WriteLine($"TAKS(2) id: {Thread.CurrentThread.ManagedThreadId}");
             return await Task.Run(() =>
             {
+                Console.WriteLine($"TAKS(2) id: {Thread.CurrentThread.ManagedThreadId}");
                 Thread.Sleep(2000);
                 return System.Math.Pow(a, 2);
             }
