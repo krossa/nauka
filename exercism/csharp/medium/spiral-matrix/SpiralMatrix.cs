1759199de4bab667207b233f6b5948823e0105dd
krossa <karol.rossa@gmail.com>
2019-11-25 11:30:18 +0100

    Exercism csharp spiral-matrix and tournament

---
+++ exercism/csharp/medium/spiral-matrix/SpiralMatrix.cs
@@ -0,0 +1,44 @@
+﻿public class SpiralMatrix
+{
+    public static int[,] GetMatrix(int size)
+    {
+        var matrix = new int[size, size];
+
+        var topRow = 0;
+        var btnRow = size - 1;
+        var leftCol = 0;
+        var rightCol = size - 1;
+        var value = 1;
+
+        while (topRow <= btnRow && leftCol <= rightCol)
+        {
+            //left to right
+            for (int j = leftCol; j <= rightCol; j++)
+                matrix[topRow, j] = value++;
+            topRow++;
+
+            //top to bottom
+            for (int i = topRow; i <= btnRow; i++)
+                matrix[i, rightCol] = value++;
+            rightCol--;
+
+            //right to left
+            if (topRow <= btnRow)
+            {
+                for (int j = rightCol; j >= leftCol; j--)
+                    matrix[btnRow, j] = value++;
+                btnRow--;
+            }
+
+            //bottom to top
+            if (leftCol <= rightCol)
+            {
+                for (int i = btnRow; i >= topRow; i--)
+                    matrix[i, leftCol] = value++;
+                leftCol++;
+            }
+        }
+
+        return matrix;
+    }
+}
---
+++ exercism/csharp/medium/tournament/Tournament.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+public static class Tournament
+{
+    public static void Tally(Stream inStream, Stream outStream)
+    {
+        var teams = GetTeams(inStream);
+        WriteResult(outStream, teams.OrderByDescending(t => t.Points).ThenBy(t => t.Name).ToList());
+    }
+
+    private static IReadOnlyList<Team> GetTeams(Stream stream)
+    {
+        var result = new List<Team>();
+        using var sr = new StreamReader(stream);
+        string line;
+        while ((line = sr.ReadLine()) != null)
+        {
+            var data = line.Split(";");
+            var team1 = GetOrAddTeam(result, data[0]);
+            var team2 = GetOrAddTeam(result, data[1]);
+            switch (data[2])
+            {
+                case "win":
+                    team1.WinCount++;
+                    team2.LossCount++;
+                    break;
+                case "loss":
+                    team1.LossCount++;
+                    team2.WinCount++;
+                    break;
+                case "draw":
+                    team1.DrawCount++;
+                    team2.DrawCount++;
+                    break;
+            }
+        }
+        return result;
+    }
+
+    private static void WriteResult(Stream stream, List<Team> teams)
+    {
+        using var sw = new StreamWriter(stream);
+        sw.Write("Team                           | MP |  W |  D |  L |  P");
+        for (int i = 0; i < teams.Count(); ++i)
+        {
+            sw.Write(Environment.NewLine);
+            sw.Write(teams.ElementAt(i).ToString());
+        }
+        sw.Flush();
+    }
+
+    private static Team GetOrAddTeam(List<Team> teams, string name)
+    {
+        if (!teams.Any(t => t.Name.Equals(name)))
+            teams.Add(new Team { Name = name });
+        return teams.First(t => t.Name.Equals(name));
+    }
+}
+
+public class Team
+{
+    public string Name { get; set; }
+    public int WinCount { get; set; }
+    public int LossCount { get; set; }
+    public int DrawCount { get; set; }
+    public int Points => WinCount * 3 + DrawCount;
+    public int MatchPlayedCount => WinCount + LossCount + DrawCount;
+
+    public override string ToString() =>
+     $"{Name,-31}|{MatchPlayedCount,3} |{WinCount,3} |{DrawCount,3} |{LossCount,3} |{Points,3}";
+}
