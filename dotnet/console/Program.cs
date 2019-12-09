f7138e462c81e05a85160224bd2af938db0ec255
krossa <karol.rossa@gmail.com>
2019-12-09 15:45:31 +0100

    some updates

--- dotnet/console/Program.cs
+++ dotnet/console/Program.cs
@@ -1,14 +1,44 @@
 ﻿using System;
 
-public class MathUtils
+public enum Side { None, Left, Right }
+
+public class ChainLink
 {
-    public static double Average(int a, int b)
+    public ChainLink Left { get; private set; }
+    public ChainLink Right { get; private set; }
+
+    public void Append(ChainLink rightPart)
     {
-        return Convert.ToDouble(a + b) / 2;
+        if (this.Right != null)
+            throw new InvalidOperationException("Link is already connected.");
+
+        this.Right = rightPart;
+        rightPart.Left = this;
     }
-    
+
+    public Side LongerSide()
+    {
+        var nextLeft = Left;
+        var nextRight = Right;
+
+        while (!(nextLeft is null) && !(nextRight is null))
+        {
+            if (nextLeft == this || nextRight == this) return Side.None;
+            nextLeft = nextLeft.Left;
+            nextRight = nextRight.Right;
+        }
+
+        if (nextRight is null && nextLeft is null) return Side.None;
+        return (nextRight is null) ? Side.Left : Side.Right;
+    }
+
     public static void Main(string[] args)
     {
-        Console.WriteLine(Average(2, 1));
+        ChainLink left = new ChainLink();
+        ChainLink middle = new ChainLink();
+        ChainLink right = new ChainLink();
+        left.Append(middle);
+        middle.Append(right);
+        Console.WriteLine(left.LongerSide());
     }
 }
