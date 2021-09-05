81615c1df4544ddfcb72e6c084c68a88e8728ee2
krossa <karol.rossa@gmail.com>
2021-09-05 19:05:20 +0200

    dotnet yield

---
+++ dotnet/sample/yield/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = GetList(3).ToList();
+            Console.WriteLine(result.Count);
+        }
+
+        private static IEnumerable<int> GetList(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}
