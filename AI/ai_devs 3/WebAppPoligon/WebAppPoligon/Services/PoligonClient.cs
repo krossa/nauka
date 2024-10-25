f8d22e0f5f15893b3f6ed7009c0c289b91dedd61
krossa <karol.rossa@gmail.com>
2024-10-25 10:28:38 +0200

    ai_devs pre task

---
+++ AI/ai_devs 3/WebAppPoligon/WebAppPoligon/Services/PoligonClient.cs
@@ -0,0 +1,37 @@
+﻿using WebAppPoligon.Models;
+
+namespace WebAppPoligon.Services
+{
+    public class PoligonClient
+    {
+        private readonly HttpClient _client;
+
+        public PoligonClient(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<string> GetDataAsync()
+        {
+            var response = await _client.GetAsync("dane.txt");
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            return result;
+        }
+
+        public async Task<string> VerifyDataAsync(VerifyRequest request)
+        {            
+           // var content = JsonContent.Create(request);
+            //content.Headers.Add("Content-Type", "application/json");
+            //var response = await _client.SendAsync("verify", content);
+
+            var response = await _client.PostAsJsonAsync("verify", request);
+
+            var parsedResponse = await response.Content.ReadAsStringAsync();
+
+            return parsedResponse;
+        }
+
+    }
+}
